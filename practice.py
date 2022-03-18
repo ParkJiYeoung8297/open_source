d2a3760fa83d6dd3328ee5edec3fa18fd78e54ba
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2022-03-18 15:49:36 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,22 @@
+#2주차 이론실습(이 #이 주석달기임)
+num1=10*2-7/3
+num2=10*(2-7)/3
+num3=1+1*3-2/2
+print(num1)
+print(num2)
+print(num3)
+
+
+num1=11/5      #정수로 입력해도 결과는 실수로 나옴
+num2=11//5     #이건 몫
+num3=11%5      #이건 나머지
+print(num1)
+print(num2)
+print(num3)
+
+sentence='Life is good'
+print(sentence)
+
+sentence=input() #이거는 입력을 받는 값
+sentence=input("이름을 입력 하세요 ") #입력 받기 전에 나오는 멘트 지정
+print(sentence) #마지막에 sentence=input("이름을 입력하세요")니까 입력받은 이름만 출력 하는거,그 위에 input 값은 출력x

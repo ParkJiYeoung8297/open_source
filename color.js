4931ebfea66ffffcb4c58be236f8772faee93379
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2022-06-03 02:35:52 +0900

    Add files via upload

--- color.js
+++ color.js
@@ -3,32 +3,15 @@ const combinationTitle = getElem("#main-picked-title");
 const pickedColorDiv = getElem("#picked-color-div");
 const pickedColorBox = getElem("#picked-color-box");
 const combiPaletteDiv = getElem("#combi-color-div");
-const combinationColorTitle = getElem("#combi-color-title");
-const combinationColorCode = getElem("#combi-color-code");
-//const makeCombinaionColorBoxToColored=getElem("#combi-color-box")
+
 /**
  * make #main-picked-title to `Your Color: ${color} on ${loc}`
  * @param elem - \#main-picked-title
  * @param {string} color - picked color
  * @param {string} loc - picked loc
  */
-
-/**
- * make #combi-color-title to `${color} ${colorCode}`
- * @param elem - \#combi-color-title
- * @param {string} color - picked color
- * @param {string} colorCode - picked colorCode
- */
-
 const makeCombinationTitle = (elem, color, loc) => {
     elem.innerText += ` ${color} on ${loc}`;
-}
-
-const makeCombinaionColorTitle=(elem, color)=>{       /////추가
-    elem.innerText += `${color}` ;
-}
-const makeCombinaionColorCode=(elem, pickedColorCode)=>{    //추가
-    elem.innerText += `${pickedColorCode}`;
 }
 
 /**
@@ -72,23 +55,14 @@ const makePickedColorBoxToColored = (elem, colorCode) => {
         elem.style.backgroundColor = `#${colorCode}`;
     }
 }
-const makeCombinaionColorBoxToColored = (elem, colorCode) => {
-    if (colorCode.toLowerCase() === "ffffff") {
-        elem.style.borderStyle = "solid";
-        elem.style.borderWidth = "1px";
-        elem.style.borderColor = "#000000";
-    } else {
-        elem.style.backgroundColor = `#${colorCode}`;
-    }
-}
 
 // Run Async Code Here
 (async () => {
     const params = extractQuery(getCurrentUrl());
     const color = params.get("color");      // picked color name
     const loc = params.get("loc");          // picked loc ("TOP" or "BOTTOM")
-    makeCombinationTitle(combinationTitle, color, loc);
 
+    makeCombinationTitle(combinationTitle, color, loc);
 
     if (isValidParam(color, loc)) {
         try {
@@ -108,23 +82,8 @@ const makeCombinaionColorBoxToColored = (elem, colorCode) => {
             const pickedColorCode = getColorCode(color);
             makePickedColorBoxToColored(pickedColorBox, pickedColorCode);
 
-            
-            makeCombinaionColorBoxToColored(pickedColorBox, pickedColorCode);
             // ------------- ADD YOUR CODE HERE --------------
             // TODO: MAKE COMBINATION COLOR LIST FROM combinationList
-    //const color = params.get("color");
-    const colorCode = params.get("colorCode");
-    makeCombinaionColorTitle(combinationColorTitle, color);
-    makeCombinaionColorCode(combinationColorCode, pickedColorCode);
-            // const makeCombinaionColorBoxToColored = (elem, colorCode) => {
-            //     if (colorCode.toLowerCase() === "ffffff") {
-            //         elem.style.borderStyle = "solid";
-            //         elem.style.borderWidth = "1px";
-            //         elem.style.borderColor = "#000000";
-            //     } else {
-            //         elem.style.backgroundColor = `#${colorCode}`;
-            //     }
-            // }
 
         } catch (e) {
             console.log(e);
